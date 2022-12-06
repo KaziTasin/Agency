9d988be31d7004c03e1128d523ac8f92a28f0fe9
KaziTasin <rananr172@gmail.com>
2022-12-06 10:00:11 +0600

    Agenc

---
+++ js/script.js
@@ -0,0 +1,11 @@
+AOS.init();
+$("#review .review-main").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "0",
+  arrows: false,
+  dots: true,
+  autoplay:true
+});
